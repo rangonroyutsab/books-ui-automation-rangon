e26094a3a48b0a3eaee8121b24569a61c2dcd2ef
Rangon Roy Utsab <rangonroy.rzs17@gmail.com>
2026-06-29 02:24:12 +0600

    formatting

--- utils/url_utils.py
+++ utils/url_utils.py
@@ -2,9 +2,9 @@ from urllib.parse import urljoin
 
 
 def build_absolute_url(base_url: str, href: str) -> str:
-    """ 
-    Generates an absolute URL 
-    by joining a base URL with a relative href. 
+    """
+    Generates an absolute URL
+    by joining a base URL with a relative href.
     """
 
     return urljoin(base_url, href)
